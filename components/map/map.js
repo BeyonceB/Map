a0b104a2d67c3d954c334bd894e4b84992eee32c
= <bbrowne14@my.bcit.ca>
2023-01-05 23:45:03 -0800

    changes

--- components/map/map.js
+++ components/map/map.js
@@ -60,7 +60,7 @@ export default function Map(){
 
     
     return(
-        <MapContainer className={style.map} center={position} zoom={12} scrollWheelZoom={true}>
+        <MapContainer className={style.map} center={[49.20006, -122.94922]} zoom={11} scrollWheelZoom={true}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
